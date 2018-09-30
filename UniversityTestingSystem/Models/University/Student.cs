0498d635d9382d0f1b5f5e2c0056d5804911adf0
Kamil <ushurbakiev.k@gmail.com>
2018-09-30 19:43:13 +0600

    AddedModel_University.Student

---
+++ UniversityTestingSystem/Models/University/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using UniversityTestingSystem.Models.Utility;
+
+namespace UniversityTestingSystem.Models.University
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.RequiredTextBox)]
+        [Display(Name = DisplayNames.FirstName)]
+        [StringLength(255, ErrorMessage = ErrorMessages.StringLength255)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.RequiredTextBox)]
+        [Display(Name = DisplayNames.LastName)]
+        [StringLength(255, ErrorMessage = ErrorMessages.StringLength255)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.RequiredComboBox)]
+        [Display(Name = DisplayNames.Group)]
+        public int GroupId { get; set; }
+
+        public Group Group { get; set; }
+
+        public bool IsFormFilled { get; set; } = false;
+    }
+}
