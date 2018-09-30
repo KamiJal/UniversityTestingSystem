89fd890aca3516a3ca619b8b104fb19cf168c5b7
Kamil <ushurbakiev.k@gmail.com>
2018-09-30 15:58:12 +0600

    AddedModel_Utility.ErrorMessages

---
+++ UniversityTestingSystem/Models/Utility/ErrorMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityTestingSystem.Models.Utility
+{
+    public static class ErrorMessages
+    {
+        public const string StringLength255 = "Длина строки не должна превышать 255 знаков";
+        public const string RequiredTextBox = "Это поле обязательно для заполнения";
+        public const string RequiredComboBox = "Вам необходимо выбрать значение";
+    }
+}
