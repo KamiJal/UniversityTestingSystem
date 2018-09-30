5b6724c8c55fb13b605d96fb9b175567429aa5d9
Kamil <ushurbakiev.k@gmail.com>
2018-09-30 16:02:41 +0600

    AddedModel_University.Faculty

---
+++ UniversityTestingSystem/Models/University/Faculty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using UniversityTestingSystem.Models.Utility;
+
+namespace UniversityTestingSystem.Models.University
+{
+    public class Faculty
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = DisplayNames.Faculty)]
+        [StringLength(255, ErrorMessage = ErrorMessages.StringLength255)]
+        public string Name { get; set; }
+    }
+}
