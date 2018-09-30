6f937a70baab9d1d63c9deb92fbb17a4f92e2dac
Kamil <ushurbakiev.k@gmail.com>
2018-09-30 15:59:09 +0600

    AddedModel_Utility.DisplayNames

---
+++ UniversityTestingSystem/Models/Utility/DisplayNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityTestingSystem.Models.Utility
+{
+    public static class DisplayNames
+    {
+        //university
+        public const string Faculty = "Факультет";
+        public const string Group = "Группа";
+
+        //student
+        public const string FirstName = "Имя";
+        public const string LastName = "Фамилия";
+        public const string AcademicPerformance = "Успеваемость";
+
+        //subjects
+        public const string Physics = "Физика";
+        public const string Math = "Математика";
+        public const string History = "История";
+        public const string ComputerScience = "Информатика";
+    }
+}
