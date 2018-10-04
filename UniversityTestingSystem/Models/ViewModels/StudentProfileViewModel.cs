01114ec7fb61e57796b89726eaddeb79031c277d
Kamil <ushurbakiev.k@gmail.com>
2018-10-04 21:55:18 +0600

    AddedModel_University.Test

---
+++ UniversityTestingSystem/Models/ViewModels/StudentProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityTestingSystem.Models.University;
+
+namespace UniversityTestingSystem.Models.ViewModels
+{
+    public class StudentProfileViewModel
+    {
+        public Student Student { get; set; }
+
+    }
+}
